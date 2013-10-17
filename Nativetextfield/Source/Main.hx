d3df26e5dba7d4e19b94bb3ac34d16e15c570c0d
wwwins <jacky.huang@isobar.com>
2013-10-17 17:50:28 +0800

    Add new icon and set new background color.

--- Nativetextfield/Source/Main.hx
+++ Nativetextfield/Source/Main.hx
@@ -19,21 +19,22 @@ class Main extends Sprite {
 	private function init():Void {
 
 		_btn = new Sprite();
-		_btn.graphics.beginFill(0xFEDCBA, 1);
-		_btn.graphics.drawRoundRect(0, 0, 100, 50, 12, 12);
+		_btn.graphics.beginFill(0x00393B, 1);
+		_btn.graphics.drawRoundRect(0, 0, 100, 36, 12, 12);
 		_btn.graphics.endFill();
 		_btn.x = _btn.y = 10;
 		addChild(_btn);
 
 		var format:TextFormat = new TextFormat();
-		format.color = 0x0000FF;
+		format.font = "Arial";
+		format.color = 0x44A18E;
 		format.size = 16;
 
 		var tf:TextField = new TextField();
 		tf.defaultTextFormat = format;
 		tf.text = "click me";
 		tf.x = (100-tf.textWidth)*.5;
-		tf.y = (50-tf.textHeight)*.5;
+		tf.y = (36-tf.textHeight)*.5;
 		_btn.addChild(tf);
 
 		_btn.addEventListener(MouseEvent.CLICK, show);
